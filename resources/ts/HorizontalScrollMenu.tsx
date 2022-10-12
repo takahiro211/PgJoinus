cdf78f3ec47f0cea908719754b32d33f50745fc6
takahiro211 <ytakahiro211@gmail.com>
2022-10-13 01:57:22 +0900

    Add app bar

---
+++ resources/ts/HorizontalScrollMenu.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+export default function ScrollableTabsButtonPrevent() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons={false}
+        aria-label="scrollable prevent tabs example"
+        TabIndicatorProps={{style: {background:'white'}}}
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
+    </Box>
+  );
+}
--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -2,14 +2,20 @@ import "../js/bootstrap";
 import "../css/app.css";
 import React from "react";
 import SignIn from './SignIn';
-import { Container, } from "@mui/material";
+import AppBar from './AppBar';
+import Drawer from './Drawer';
+import { Container, Box, } from "@mui/material";
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
     return (
-      <Container maxWidth="sm" >
-        <SignIn />
-      </Container>
+      <Box>
+        <AppBar />
+        <Drawer />
+        <Container maxWidth="sm" >
+          <SignIn />
+        </Container>
+      </Box>
     );
 };
 const root = createRoot(document.getElementById('app') as HTMLElement);
